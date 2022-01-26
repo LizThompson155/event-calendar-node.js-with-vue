53be9c36156d290d91f30adb90f6e0f35949f2ae
Elizabeth Thompson <30391404+LizThompson155@users.noreply.github.com>
2022-01-26 13:25:04 -0500

    initial commit

---
+++ src/data/events/getEvents.sql
@@ -0,0 +1,10 @@
+SELECT [id]
+		,[title]
+		,[description]
+		,[startDate]
+		,[startTime]
+		,[endDate]
+		,[endTime]
+FROM	[dbo].[events] 
+WHERE [userId] = @userId
+ORDER BY [startDate], [startTime]
